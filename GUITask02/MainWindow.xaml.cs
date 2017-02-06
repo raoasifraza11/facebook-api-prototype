9075b882a4f7bf376914568d7c4d6cda13a3d379
AsifRaza <AsifRaza@ASIF-PC>
2017-02-06 20:04:59 +0500

    Adone facebook api version 7.0.6

--- GUITask02/MainWindow.xaml.cs
+++ GUITask02/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Facebook;
 
 namespace GUITask02
 {
@@ -24,5 +25,8 @@ namespace GUITask02
         {
             InitializeComponent();
         }
+
+
+        
     }
 }
