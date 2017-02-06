ee5eddc6092dc240301b89eaeccb46276115793d
AsifRaza <AsifRaza@ASIF-PC>
2017-02-06 21:08:41 +0500

    Fetch images, personl detail and friends list

--- GUITask02/MainWindow.xaml.cs
+++ GUITask02/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Facebook;
+using System.Net;
+using System.IO;
 
 namespace GUITask02
 {
@@ -29,7 +31,7 @@ namespace GUITask02
         {
             InitializeComponent();
             GetFriendList();
-            getAbout();
+            GetUserDetails();
         }
 
 
@@ -54,7 +56,7 @@ namespace GUITask02
         }
 
 
-        public void getAbout()
+        public void GetUserDetails()
         {
             client = new FacebookClient();
             client.AccessToken = "EAACEdEose0cBANVH1bjGJKlLoRM8bpyRdnlmHg1UZCA044TjKFIT1VrN2x4TpoYBOdMLihwQ7PTjnD0Y92FYWxMms2K6TjrUkip5jZCkQ2A47JZBTMZBUyp86VvyJe5pIEDRwdkqCNeVRvfwpPZC0U2ztvWNXKSnAmYHhONK6cxZAGZBg6tFZCWVczekiuKmBzMZD";
@@ -62,6 +64,34 @@ namespace GUITask02
             user_name.Content = Me.name;
             user_dob.Content = Me.birthday;
             user_email.Content = Me.email;
+            string url = Me.picture.data.url;
+
+            var image = new BitmapImage();
+            int BytesToRead = 100;
+
+            WebRequest request = WebRequest.Create(new Uri(url, UriKind.Absolute));
+            request.Timeout = -1;
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            BinaryReader reader = new BinaryReader(responseStream);
+            MemoryStream memoryStream = new MemoryStream();
+
+            byte[] bytebuffer = new byte[BytesToRead];
+            int bytesRead = reader.Read(bytebuffer, 0, BytesToRead);
+
+            while (bytesRead > 0)
+            {
+                memoryStream.Write(bytebuffer, 0, bytesRead);
+                bytesRead = reader.Read(bytebuffer, 0, BytesToRead);
+            }
+
+            image.BeginInit();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            image.StreamSource = memoryStream;
+            image.EndInit();
+
+            user_image.Source = image;
         }
 
     }
